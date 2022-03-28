d96c254140494d4f5204e34f7a24e98492f2f622
NahrulK <khayattullah73@gmail.com>
2022-03-28 15:22:17 +0800

    Navbar Components

--- src/pages/components/Navbar.jsx
+++ src/pages/components/Navbar.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Badge from "@material-ui/core/Badge";
+import { SearchOutlined, ShoppingCartOutlined } from "@material-ui/icons";
+
+const Container = styled.div`
+  height: 60px;
+`;
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const SearchContainer = styled.div`
+  border: solid 0.5px lightgray;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Center = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-item: center;
+  justify-content: flex-end;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>ID</Language>
+          <SearchContainer>
+            <Input />
+            <SearchOutlined style={{ color: "gray", fontSize: 16 }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>GETAMA.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
