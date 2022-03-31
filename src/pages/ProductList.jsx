b4fa587e2f08be6d99162ef5543571da7eddeb08
NahrulK <khayattullah73@gmail.com>
2022-03-31 14:03:39 +0800

    Product List Pages

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,80 @@
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  margin-right: 20px;
+`;
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Produk: </FilterText>
+          <Select>
+            <Option disabled selected>
+              - Kategori Barang -
+            </Option>
+            <Option>Genteng </Option>
+            <Option>Batu Bata</Option>
+            <Option>Gerabah</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              - Jenis Barang -
+            </Option>
+            <Option>Genteng Buwung</Option>
+            <Option>Genteng Kecil</Option>
+            <Option>Genteng Besar</Option>
+            <Option>Genteng Bali</Option>
+            <Option>Genteng Mentah</Option>
+            <Option>Batu Bata Biasa</Option>
+            <Option>Batu Bata Premuim</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Urutkan: </FilterText>
+          <Select>
+            <Option selected>Paling Sesuai</Option>
+            <Option>Terbaru</Option>
+            <Option>Harga Terendah</Option>
+            <Option>harga Tertinggi</Option>
+            <Option>Jenis Produk</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
