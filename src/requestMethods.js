e26778a80a31ef63dbc32e9655041b616c8d6c48
NahrulK <khayattullah73@gmail.com>
2022-05-26 11:44:09 +0800

    Stripe payment key solved

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
 const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2I3OTFhMmIxNjk5YzZlZGFjYjY5MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MzQ1OTk4MiwiZXhwIjoxNjUzNzE5MTgyfQ.uNrTVrC2M286-5brcQDaYP4oAR1Ex61mYazvkyF-wBg";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyN2I3OTFhMmIxNjk5YzZlZGFjYjY5MSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1MzUzMzI3NCwiZXhwIjoxNjUzNzkyNDc0fQ.0V2EoduLW5UB4j0tQPL-4Wfutkrnf_SSx_9iC_eyO2Y";
 
 export const publicRequest = axios.create({ baseURL: BASE_URL });
 
